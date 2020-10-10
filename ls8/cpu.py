7426763253bdfe896c2c85bd5875fb569beb73b1
Gideon <gideon@mac-1s-mbp.myfiosgateway.com>
2020-10-09 22:29:23 -0400

    coded ram_read() and ram_write() functions

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,15 +2,25 @@
 
 import sys
 
+HLT = 0b00000001 
+LDI = 0b10000010
+
 class CPU:
     """Main CPU class."""
 
     def __init__(self):
         """Construct a new CPU."""
         pass
+        self.ram = [0] * 256
+
+        self.registers = [0] * 8
+
+        self.registers[7] = 0xF4
+
         self.pc = 0
-        self.reg = [0] * 8
-        self.ram = [0] * 8
+
+        
+        
 
     def load(self):
         """Load a program into memory."""
@@ -34,42 +44,57 @@ class CPU:
             self.ram[address] = instruction
             address += 1
 
+    def ram_read(self, MAR):
+        return self.ram[MAR]
+
+
+    def ram_write(self, MAR, MDR):
+        self.ram[MAR] = MDR
+
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
 
         if op == "ADD":
-            self.reg[reg_a] += self.reg[reg_b]
+            self.registers[reg_a] += self.registers[reg_b]
         #elif op == "SUB": etc
         else:
             raise Exception("Unsupported ALU operation")
-        
-    def ram_read(self, prop_pc):
-        pass
 
-    def ram_write(self, prop_pc):
-        pass
+    # def trace(self):
+    #     """
+    #     Handy function to print out the CPU state. You might want to call this
+    #     from run() if you need help debugging.
+    #     """
 
-    def trace(self):
-        """
-        Handy function to print out the CPU state. You might want to call this
-        from run() if you need help debugging.
-        """
+    #     print(f"TRACE: %02X | %02X %02X %02X |" % (
+    #         self.pc,
+    #         #self.fl,
+    #         #self.ie,
+    #         self.ram_read(self.pc),
+    #         self.ram_read(self.pc + 1),
+    #         self.ram_read(self.pc + 2)
+    #     ), end='')
 
-        print(f"TRACE: %02X | %02X %02X %02X |" % (
-            self.pc,
-            #self.fl,
-            #self.ie,
-            self.ram_read(self.pc),
-            self.ram_read(self.pc + 1),
-            self.ram_read(self.pc + 2)
-        ), end='')
+    #     for i in range(8):
+    #         print(" %02X" % self.reg[i], end='')
 
-        for i in range(8):
-            print(" %02X" % self.reg[i], end='')
-
-        print()
+    #     print()
 
     def run(self):
         """Run the CPU."""
-        pass
+        running = True
+        while running:
+            IR = self.ram_read(self.pc)
+            operand_a = self.ram_read(self.pc + 1)
+            operand_b = self.ram_read(self.pc + 2)
+
+            num_operands = IR >> 6
+            self.pc += 1 + num_operands
+            
+            if IR == HLT:
+                running = False
+            
+            # elif IR == LDI:
+                
+
