4461ca9de4aa496244bdb30c6b49b525cb985c82
GideobOgbuagu <reachgiddy@gmail.com>
2020-10-12 23:56:09 -0400

    MVP update 3

--- ls8/ls8.py
+++ ls8/ls8.py
@@ -6,6 +6,13 @@ import sys
 from cpu import *
 
 cpu = CPU()
+print("hello")
+# TODO: grab any args
+if len(sys.argv) != 2:
+    print("usage: simple.py filename")
+    sys.exit(1)
+# TODO: load opcodes in to memory
+print(sys.argv)
+cpu.load(sys.argv[1])
 
-cpu.load()
 cpu.run()
